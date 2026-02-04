5a8f430c22f647012e8ce46568e99b1ffd12770e
kernemi kidane <149039271+kernemi@users.noreply.github.com>
2026-02-04 14:25:58 +0300

    Implement method to check if two arrays are equal

---
+++ TwoArraysEqual.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkEqual(self, a, b) -> bool:
+        a.sort()
+        b.sort()
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return False
+        else:
+            return True
