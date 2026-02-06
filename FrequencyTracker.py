42e9852fa111e76a0386679e65d2b09258370a26
kernemi kidane <149039271+kernemi@users.noreply.github.com>
2026-02-06 17:12:56 +0300

    Add Frequency Tracker class with add, delete, and check methods
    Implement FrequencyTracker class to manage number frequencies.

---
+++ FrequencyTracker.py
@@ -0,0 +1,34 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        self.count = Counter()     
+        self.freq = Counter()      
+
+    def add(self, number: int) -> None:
+        old = self.count[number]
+        if old > 0:
+            self.freq[old] -= 1
+        self.count[number] += 1
+        self.freq[old + 1] += 1
+
+    def deleteOne(self, number: int) -> None:
+        old = self.count[number]
+        if old == 0:
+            return
+        self.freq[old] -= 1
+        if old == 1:
+            del self.count[number]
+        else:
+            self.count[number] -= 1
+            self.freq[old - 1] += 1
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq[frequency] > 0
+
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
