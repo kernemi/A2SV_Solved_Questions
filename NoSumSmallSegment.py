b6687deeccb754945add8e2fd263ea40dc992b92
kernemi kidane <149039271+kernemi@users.noreply.github.com>
2026-02-27 17:14:28 +0300

    Create NoSumSmallSegment.py

---
+++ NoSumSmallSegment.py
@@ -0,0 +1,16 @@
+n,k = map(int,input().split())
+s = list(map(int,input().split()))
+
+total = 0
+sums = 0
+left = 0
+
+for right in range(n):
+    total += s[right]
+    while total > k:
+        total -= s[left]
+        left += 1
+    sums += right - left + 1
+print(sums)
+
+
