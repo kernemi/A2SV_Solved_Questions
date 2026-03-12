274418cd1533503821c2d3fc50f1db634e474991
kernemi kidane <149039271+kernemi@users.noreply.github.com>
2026-03-12 18:10:24 +0300

    Add solution for Jump Game

---
+++ leetcode/0055-jump-game/0055-jump-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        count = 0
+        for x in nums:
+            if count < 0:
+                return False
+            elif x > count:
+                count = x
+            count = count - 1
+
+        return True
