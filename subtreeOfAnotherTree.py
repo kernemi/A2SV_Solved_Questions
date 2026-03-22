ca810d8a9715c357c1196eb8e9ce7090dfd7ccb0
kernemi kidane <149039271+kernemi@users.noreply.github.com>
2026-03-22 19:58:41 +0300

    Add subtree checking function in Solution class
    Implement a function to check if one binary tree is a subtree of another.

---
+++ subtreeOfAnotherTree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def issame(p,q):
+            if not p and not q:
+                return True 
+
+            if not p or not q:
+                return False 
+
+            if p.val != q.val:
+                return False
+
+            left = issame(p.left, q.left)
+            right = issame(p.right, q.right)
+
+            return left and right 
+        
+        if not subRoot:
+            return True 
+
+        if not root:
+            return False
+
+        if issame(root, subRoot):
+            return True 
+        
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+        return left or right 
